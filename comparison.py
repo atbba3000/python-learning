47c7c93c353544c5794c657806b3bbd096d29a4b
atbba3000 <83802526+atbba3000@users.noreply.github.com>
2021-05-07 21:21:26 +0530

    Add files via upload

---
+++ comparison.py
@@ -0,0 +1,9 @@
+a = int(input("enter the 1st digit"))
+b = int(input("enter the 2nd digit"))
+if a>b:
+    print("a is greater",a)
+
+elif b>a:
+    print("b is greater",b)
+else:
+    print("a equals b")
